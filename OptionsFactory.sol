fda719c345a2472ca9f0bca7c25d747b9413f30c
asifkhan261 <asifkhan261@gmail.com>
2022-09-14 17:50:33 +0530

    Changed architecture

--- OptionsFactory.sol
+++ OptionsFactory.sol
@@ -1,33 +1,47 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma abicoder v2;
 pragma solidity >=0.7.0 <0.9.0;
-contract OptionsFactory{
+contract OptionFactory{
 
     uint payOff;
+    address public owner;
     struct Position{
         bool postionFlag;
         uint strikePrice;
         uint premium;
     }
     struct Strategy{
+        uint256 strategyId;
+        StrategyDesc strategyDesc;
+        address creator;
+        bool live;
+        address[] buyers;
+        uint finalPayoff;
+        uint maxPayoff;
+        uint minPayoff;   
+    }
+    struct StrategyDesc{
         Position longCall;
         Position shortCall;
         Position longPut;
         Position shortPut;
-        uint256 marketId;   
-    }
-    struct StrategyMarket{
+        address creator;
         address token;
-        bool created;
         uint256 startAt;
         uint256 endAt;
     }
+    mapping(uint256 => Strategy) public strategy;
+    mapping(address => uint256) public creatorMapping;
+    mapping(address => uint256) public strategyCreator2;
 
-    // modifier onlyCreator{
-    //     require(msg.sender == )
-    // }
-    mapping(address => Strategy) public strategies;
-    mapping(uint256 => StrategyMarket) public StrategyMarkets;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
 
     function getCurrentPrice() public pure returns(uint){
         // call API3 here
@@ -55,7 +69,7 @@ contract OptionsFactory{
         }
         return payOff;
     }
-    function longPut(uint strikePrice,uint premium) external returns(uint) {    
+    function longPut(uint strikePrice,uint premium) internal returns(uint) {    
         uint currentPrice = getCurrentPrice();
         if( strikePrice - currentPrice > 0){
             payOff = strikePrice - currentPrice - premium;
@@ -65,7 +79,7 @@ contract OptionsFactory{
         }
         return payOff;
     }
-    function shortPut(uint strikePrice,uint premium) external returns(uint) {    
+    function shortPut(uint strikePrice,uint premium) internal returns(uint) {    
         uint currentPrice = getCurrentPrice();
         if( strikePrice - currentPrice > 0){
             payOff = premium + strikePrice - currentPrice;
@@ -76,20 +90,45 @@ contract OptionsFactory{
         return payOff;
     }
     
-    function openPostion(Strategy memory _strategy)external{
+    function openStrategy(StrategyDesc memory _strategyDesc) internal returns (uint,uint,uint){
         uint finalPayoff;
-        strategies[msg.sender] = _strategy;
-
-        if(_strategy.longCall.postionFlag == true){
-            finalPayoff += longCall(_strategy.longCall.strikePrice, _strategy.longCall.premium);
+        uint maxPayoff;
+        uint minPayoff;
+        if(_strategyDesc.longCall.postionFlag == true){
+            finalPayoff += longCall(_strategyDesc.longCall.strikePrice, _strategyDesc.longCall.premium);
         }
-        if(_strategy.shortCall.postionFlag == true){
-            finalPayoff += shortCall(_strategy.shortCall.strikePrice, _strategy.shortCall.premium);
+        if(_strategyDesc.shortCall.postionFlag == true){
+            finalPayoff += shortCall(_strategyDesc.shortCall.strikePrice, _strategyDesc.shortCall.premium);
         }
+        if(_strategyDesc.longPut.postionFlag == true){
+            finalPayoff += longPut(_strategyDesc.longPut.strikePrice, _strategyDesc.longPut.premium);
+        }
+        if(_strategyDesc.shortPut.postionFlag == true){
+            finalPayoff += shortPut(_strategyDesc.shortPut.strikePrice, _strategyDesc.shortPut.premium);
+        }
+        return (finalPayoff,maxPayoff,minPayoff);
+    }
+    function createStrategyMarket(StrategyDesc memory _strategyDesc)external  {
+        strategy[block.timestamp];
+        strategy[block.timestamp].strategyId = block.timestamp;
+        strategy[block.timestamp].strategyDesc = _strategyDesc;
+        strategy[block.timestamp].creator = msg.sender;
+        strategy[block.timestamp].live = true;
+        (strategy[block.timestamp].finalPayoff,strategy[block.timestamp].maxPayoff,strategy[block.timestamp].minPayoff) = openStrategy(strategy[block.timestamp].strategyDesc);
+        creatorMapping[msg.sender] = strategy[block.timestamp].strategyId;
+    }
+    function  buyStrategy(uint256 _strategyId,uint numberofStrat) payable public {
+        require(msg.value > strategy[_strategyId].maxPayoff * numberofStrat);
+        // to implement
 
     }
-    function createStrategyMarket(StrategyMarket memory _strategyMarket, uint256 _id)external  {
-        require(!StrategyMarkets[_id].created);
-        StrategyMarkets[_id] = _strategyMarket;
+    function  provideLPStrategy(uint256 _strategyId,uint numberofStrat) payable public {
+        require(msg.value > strategy[_strategyId].maxPayoff * numberofStrat);
+        // to implement
+
     }
+    function settle() public onlyOwner{
+        // to implement
+    }
+
 }
